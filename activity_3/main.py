c2d7981dfe2c5584efad2b4b089743db598c64ba
RJUSM <rustam190401@mail.ru>
2022-04-02 02:32:31 +0300

    activity 3, 4

---
+++ activity_3/main.py
@@ -0,0 +1,122 @@
+import pickle
+
+from typing import Dict, Set, List
+
+from utils import text_preprocessing, get_words_from_text, lemmatize_word, restruct
+
+site_db_filename = "sites_db.pickle"
+inverted_indexes_db_filename = "inverted_indexes_db.pickle"
+
+
+def save_index_file_to_pickle():
+    site_index_dict = {}
+
+    with open("../index.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            info = line.split()
+            site_index_dict[int(info[0])] = info[1]
+
+    with open(site_db_filename, 'wb') as f:
+        pickle.dump(site_index_dict, f)
+
+
+def get_sites() -> Dict[int, str]:
+    with open(site_db_filename, 'rb') as sites_db:
+        sites = pickle.load(sites_db)
+    return sites
+
+
+def create_inverted_indexes_to_pickle():
+    inverted_indexes = {}
+    sites = get_sites()
+    for site_id in sites:
+        print(f"Parse {site_id} file")
+        with open(f"../activity_1/sites/{str(site_id)}.txt", "r", encoding="utf-8") as f:
+            text = text_preprocessing(f.read())
+            words = get_words_from_text(text)
+            for word in words:
+                word_lemma = lemmatize_word(word)
+                if word_lemma in inverted_indexes:
+                    if site_id not in inverted_indexes[word_lemma]:
+                        inverted_indexes[word_lemma].append(site_id)
+                else:
+                    inverted_indexes[word_lemma] = [site_id, ]
+
+    with open(inverted_indexes_db_filename, 'wb') as f:
+        pickle.dump(inverted_indexes, f)
+
+
+def save_inverted_indexes_to_txt():
+    inverted_indexes = get_inverted_indexes()
+    with open("inverted_index.txt", "w", encoding="utf-8") as f:
+        for index in inverted_indexes:
+            indexes = " ".join(list(map(str, inverted_indexes[index])))
+            f.write(f"{index}: {indexes}\n")
+
+
+def get_inverted_indexes() -> Dict[str, List[int]]:
+    with open(inverted_indexes_db_filename, 'rb') as inverted_indexes_db:
+        inverted_indexes = pickle.load(inverted_indexes_db)
+    return inverted_indexes
+
+
+def not_operator(lemma: str, inverted_indexes: Dict[str, List[int]]) -> set:
+    sites_id = get_sites().keys()
+    response = sites_id - set(inverted_indexes.get(lemma, []))
+    return response
+
+
+def bool_search(search: str) -> Set[int]:
+    operators = ["AND", "OR", "NOT"]
+    search_new = restruct(search)
+    split_search = search_new.split()
+
+    res_q = []
+    # Если между словами пробел, то вставляем AND между ними
+    for i in range(len(split_search) - 1):
+        if split_search[i] not in operators and (split_search[i + 1] not in operators or split_search[i + 1] == "NOT"):
+            res_q.extend([split_search[i], "AND"])
+            continue
+        else:
+            res_q.append(split_search[i])
+    if split_search[-1] not in operators:
+        res_q.append(split_search[-1])
+
+    inverted_indexes = get_inverted_indexes()
+
+    query = " ".join(res_q).replace("NOT ", "!").replace("OR", "|").replace("AND", "&")
+    query_split = query.split()
+    for word in query_split:
+        if word not in ["&", "|"]:
+            if word.startswith("!"):
+                query = query.replace(word, str(set(not_operator(word[1:], inverted_indexes))))
+            elif word.startswith("("):
+                query = query.replace(word[1:], str(set(inverted_indexes.get(word[1:], []))))
+            elif word[-1] == ")":
+                query = query.replace(word[:-1], str(set(inverted_indexes.get(word[:-1], []))))
+            else:
+                query = query.replace(word, str(set(inverted_indexes.get(word, []))))
+    try:
+        result = eval(query)
+    except Exception:
+        result = set()
+    return result
+
+
+def main():
+    # save_index_file_to_pickle()
+    # create_inverted_indexes_to_pickle()
+    # save_inverted_indexes_to_txt()
+    search_query = input("Найти: ")
+    sites = get_sites()
+    idxs = bool_search(search_query)
+    result_sites = [sites.get(idx) for idx in idxs]
+    print(
+        {
+            "query": search_query,
+            "results": result_sites
+        }
+    )
+
+
+main()
---
+++ activity_3/utils.py
@@ -0,0 +1,77 @@
+import pymorphy2
+from typing import List
+
+
+def pos(word: str, morth=pymorphy2.MorphAnalyzer()) -> str:
+    # функция возвращает часть речи слова
+    return morth.parse(word)[0].tag.POS
+
+
+def text_preprocessing(input_text: str) -> str:
+    # убираем знаки пунктуации и числа; приводим к нижнему регистру
+    punctuation = """!"#$%&\'()*+,.:;<=>?@[\\]^_`{|}~"""
+    tt = str.maketrans(dict.fromkeys(f"{punctuation}“”«»"))
+    functors_pos = {'CONJ', 'PREP', 'PRCL', 'INTJ'}
+    output = input_text.lower().translate(tt).replace("/", " ")
+    output_splitted = output.split()
+    for word in output_splitted:
+        if pos(word) in functors_pos or word in ["–", "", " "] or is_digit(word):
+            output = output.replace(word, "")
+    return output
+
+
+def is_digit(str_input: str) -> bool:
+    # проверяем есть ли в слове цифра
+    for el in str_input:
+        try:
+            float(el)
+            return True
+        except ValueError:
+            pass
+    return False
+
+
+def get_words_from_text(input_text: str) -> List[str]:
+    """
+    Разбиваем текст на слова; обрезаем пробелы вокруг слова
+    убираем служебные части речи
+    """
+    # INTJ - междометие; PRCL - частица; CONJ - союз; PREP - предлог
+    functors_pos = {'CONJ', 'PREP', 'PRCL', 'INTJ'}
+    words = list(map(lambda word: word.strip(), input_text.split()))
+    words = [word for word in words if
+             pos(word) not in functors_pos and word not in ["–", "", " "] and not is_digit(word)]
+    return words
+
+
+def lemmatize_word(word: str) -> str:
+    """
+    Метод для получения леммы
+    """
+    morph = pymorphy2.MorphAnalyzer()
+    p = morph.parse(word)[0]
+    return p.normal_form
+
+
+def restruct(input_text: str) -> str:
+    """
+    Метод, который убирает из текста пунктуацию, служебные части речи,
+    И приводит каждое слово к нижнему регистру, и заменяет на лемму
+    """
+    operators = ["AND", "OR", "NOT"]
+    split_text = input_text.split()
+    punctuation = """!"#$%&\'*+,.:;<=>?@[\\]^_`{|}~"""
+    tt = str.maketrans(dict.fromkeys(f"{punctuation}“”«»"))
+    functors_pos = {'CONJ', 'PREP', 'PRCL', 'INTJ'}
+    text = []
+    for el in split_text:
+        if el in operators:
+            text.append(el)
+        elif pos(el) not in functors_pos or el in ["–", "", " "] or is_digit(el):
+            if el.startswith("("):
+                text.append("(" + lemmatize_word(el[1:].lower().translate(tt).replace("/", " ")))
+            elif el[-1] == ")":
+                text.append(lemmatize_word(el[:-1].lower().translate(tt).replace("/", " ")) + ")")
+            else:
+                text.append(lemmatize_word(el.lower().translate(tt).replace("/", " ")))
+    return " ".join(text)
